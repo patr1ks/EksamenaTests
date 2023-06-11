e3c7e0d0df8e235ea3d51ee4804f44899f857cef
Patriks Venens <patriks.venens@gmail.com>
2023-06-11 04:19:55 +0300

    Pievienoti jautājumi

--- mainTests.cpp
+++ mainTests.cpp
@@ -18,9 +18,39 @@ do{
 }while(atbilde == false);
 
 do{
-    cout<<"vai es gribu gulet\n\n1) ja \n2) ne\n3) protams\n4) ofc\n\n Atbilde: ";
+    cout<<"Kurus no siem operatoriem var izmantot dalisanai C++?\n\n1) \\\n2) *\n3) %\n4) &\n\n Atbilde: ";
     cin>>atb;
-    if(atb == 2){
+    if(atb == 2 || atb == 4){
+        atbilde=false;
+    }else{
+        atbilde=true;
+    }
+}while(atbilde == false);
+
+do{
+    cout<<"Ka inicializet mainigo C++?\n\n1) int a; a(10);\n2) int a (10)\n3) int a = 10;\n4) int a; a=10;\n\n Atbilde: ";
+    cin>>atb;
+    if(atb == 1 || atb == 2 ){
+        atbilde=false;
+    }else{
+        atbilde=true;
+    }
+}while(atbilde == false);
+
+do{
+    cout<<"Kurs no siem operatoriem tiek izmantots, lai salidzinatu divas vienadibas vertibas C++?\n\n1) ==\n2) !=\n3) =\n4) ||\n\n Atbilde: ";
+    cin>>atb;
+    if(atb == 3 || atb == 4 ){
+        atbilde=false;
+    }else{
+        atbilde=true;
+    }
+}while(atbilde == false);
+
+do{
+    cout<<"Kuri no siem ir derigi veidi, ka izvadit mainiga x vertibu?\n\n1) cout<<x;\n2) cout>>x;\n3) printf(x);\n4) cout<<\"Skaitla vertiba ir\"<<x;\n\n Atbilde: ";
+    cin>>atb;
+    if(atb == 2 || atb == 3 ){
         atbilde=false;
     }else{
         atbilde=true;
